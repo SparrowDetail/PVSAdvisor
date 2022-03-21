f139224565937f10dc4044dd170427e95b4ad5e1
Steve <sshort95@gmailt.com>
2022-03-21 15:02:42 -0400

    Realm SDK Plugin implementation in the Gradle. May require re-sync of the Gradle (File>Sync Project with Gradle Files).

--- app/src/main/java/com/pvs/pvsadvisor/MainActivity.kt
+++ app/src/main/java/com/pvs/pvsadvisor/MainActivity.kt
@@ -9,6 +9,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //test
+        setContentView(R.layout.activity_main)
     }
 }
