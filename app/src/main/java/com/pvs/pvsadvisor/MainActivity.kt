5cb252cdd2ffe5f36293c12705f46d2c87a54077
Steve <sshort95@gmailt.com>
2022-03-27 04:23:01 -0400

    NOTE: Style and String resources missing for activity_profile.xml Additions/Changes:  - Set user_home.xml as MainActivity.kt layout file  - Implemented a menu and inflator, which will point to profile from user_Home  - Renamed PVSLogoDiamond.png in accordance to required naming conventions: pvs_logo_diamond.png  - Added upload package
     New files:
      - user_home_menu.xml
      - pvs_logo_diamond.png

--- app/src/main/java/com/pvs/pvsadvisor/MainActivity.kt
+++ app/src/main/java/com/pvs/pvsadvisor/MainActivity.kt
@@ -2,6 +2,9 @@ package com.pvs.pvsadvisor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
 /*
  * Main function for Prime Vice Advisor.
  * Developed by Amar, Steven, and Jacob.
@@ -9,6 +12,22 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.user_home)
+    }
+
+    //Inflates the custom menu from menu asset
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.user_home_menu, menu)
+        return true
+    }
+
+    //Controls menu actions on home page
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.user_home_profile_directory) {
+            //TODO:Direct the user to the profile page
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
