321acad983455e03118d29029d6c33536aad8893
Steve <sshort95@gmailt.com>
2022-03-28 03:00:55 -0400

    Progress on upload main fragment implementation to control transition steps of upload process. New Files:  - UploadCategoryFragment.kt  - upload_main_activity.xml

---
+++ app/src/main/java/com/pvs/pvsadvisor/upload/UploadCategoryFragment.kt
@@ -0,0 +1,6 @@
+package com.pvs.pvsadvisor.upload
+
+import androidx.fragment.app.Fragment
+import com.pvs.pvsadvisor.R
+
+class UploadCategoryFragment: Fragment(R.layout.upload_category_select)
--- app/src/main/java/com/pvs/pvsadvisor/upload/UploadMain.kt
+++ app/src/main/java/com/pvs/pvsadvisor/upload/UploadMain.kt
@@ -2,6 +2,9 @@ package com.pvs.pvsadvisor.upload
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.pvs.pvsadvisor.R
 
 /**
@@ -11,7 +14,31 @@ import com.pvs.pvsadvisor.R
 class UploadMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.upload_catagory_select)
+        //TODO: Implement content view progress in DB
+        setContentView(R.layout.upload_main_activity)
+
+        //Implement fragment data
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<UploadCategoryFragment>(R.id.uploadMainFragmentView)
+            }
+        }
+
+        //Activity save button
+        val fragSaveButton = findViewById<Button>(R.id.uploadSaveButton)
+        fragSaveButton.setOnClickListener {
+            //TODO: Save project data to Database
+            finish()
+        }
+
+        val fragNextButton = findViewById<Button>(R.id.uploadNextButton)
+        fragNextButton.setOnClickListener {
+            //TODO: Set view data to Database
+
+        }
     }
+
+
 
 }
