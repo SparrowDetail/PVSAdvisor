9457f4f79579647b5a8d3da9f4b26abed5cecc55
Amar Sapcanin <trek0022@gmail.com>
2022-03-29 10:01:39 -0400

    Removed Action Bar added splash file.

---
+++ app/src/main/java/com/pvs/pvsadvisor/Splash.kt
@@ -0,0 +1,22 @@
+package com.pvs.pvsadvisor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+/*
+ * Developed by Amar, Steven, and Jacob.
+ */
+class Splash : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
