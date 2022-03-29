8024d5c90eaa333dc2ec30ca1840a8fc43757239
cheesydance <cheesydance@outlook.com>
2022-03-29 18:14:28 -0400

    Description: - Redesigned Profile Page - Profile Settings Fragment - App Icons of various sizes.

--- app/src/main/java/com/pvs/pvsadvisor/profile/MainProfile.kt
+++ app/src/main/java/com/pvs/pvsadvisor/profile/MainProfile.kt
@@ -1,4 +1,17 @@
 package com.pvs.pvsadvisor.profile
 
-class MainProfile {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.pvs.pvsadvisor.R
+
+class MainProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_profile)
+
+        btn_EditProfile
+    }
 }
---
+++ app/src/main/java/com/pvs/pvsadvisor/profile/UserSettingsFragment.kt
@@ -0,0 +1,21 @@
+package com.pvs.pvsadvisor.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import com.pvs.pvsadvisor.R
+
+public class UserSettingsFragment : Fragment(R.layout.dialogfragment_profilesettings) {
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.dialogfragment_profilesettings, container, false)
+//    }
+
+
+}
