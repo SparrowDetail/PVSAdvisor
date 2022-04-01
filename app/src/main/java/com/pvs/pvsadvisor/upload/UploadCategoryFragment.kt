e2e7368654f187203124eb2afccafb9160fa5c02
Steve <sshort95@gmailt.com>
2022-04-01 16:29:09 -0400

    Progress on core functionality on UploadCategoryFragment.kt

--- app/src/main/java/com/pvs/pvsadvisor/upload/UploadCategoryFragment.kt
+++ app/src/main/java/com/pvs/pvsadvisor/upload/UploadCategoryFragment.kt
@@ -30,7 +30,11 @@ class UploadCategoryFragment : Fragment(R.layout.fragment_upload_category) {
         val nextBtn = view.findViewById<Button>(R.id.uploadNextButton_cat)
         val saveBtn = view.findViewById<Button>(R.id.uploadSaveButton_cat)
         nextBtn.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.navigate_to_title_selection)
+            //Requires the user to select at least one category
+            if (boxesChecked > 0)
+                Navigation.findNavController(view).navigate(R.id.navigate_to_title_selection)
+            else
+                Toast.makeText(view.context,getString(R.string.category_more_options),Toast.LENGTH_SHORT).show()
         }
         saveBtn.setOnClickListener {
             //TODO: Save activity data
@@ -44,19 +48,46 @@ class UploadCategoryFragment : Fragment(R.layout.fragment_upload_category) {
         when (checkbox.id) {
             R.id.cat_char_design -> {
                 if (checkbox.isChecked) {
-                    Toast.makeText(view.context, "Checked", Toast.LENGTH_LONG).show()
-                    selectedCategories += "Category,"
+                    Toast.makeText(view.context, selectedCategories, Toast.LENGTH_LONG).show()
+                    selectedCategories += "Character Design,"
+                    boxesChecked += 1
+                } else {
+                    Toast.makeText(view.context, "Not Category", Toast.LENGTH_LONG).show()
+                    selectedCategories.replace("Character Design,", "")
                     boxesChecked -= 1
                 }
-                else {
-                    Toast.makeText(view.context,"Not Checked", Toast.LENGTH_LONG).show()
-
+            }
+            R.id.cat_storyboarding -> {
+                if (checkbox.isChecked) {
+                    selectedCategories += "Storyboarding,"
+                    boxesChecked += 1
+                } else {
+                    selectedCategories.replace("Storyboarding,", "")
+                    boxesChecked -= 1
+                }
+            }
+            R.id.cat_seq_art -> {
+                if (checkbox.isChecked) {
+                    selectedCategories += "Sequential Art,"
+                    boxesChecked += 1
+                } else {
+                    selectedCategories.replace("Sequential Art,", "")
+                    boxesChecked -= 1
+                }
+            }
+            R.id.cat_com_book -> {
+                if (checkbox.isChecked) {
+                    selectedCategories += "Comic Book,"
+                    boxesChecked += 1
+                } else {
+                    selectedCategories.replace("Sequential Art,", "")
                     boxesChecked -= 1
                 }
             }
         }
-    }
 
+
+    }
 
     override fun onDestroy() {
         super.onDestroy()
