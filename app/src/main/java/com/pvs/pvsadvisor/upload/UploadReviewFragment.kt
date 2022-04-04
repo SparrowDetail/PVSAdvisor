24789f8eec41871adce3998b39a7bf725ab20ce1
Steve <sshort95@gmailt.com>
2022-04-04 02:05:19 -0400

    UploadReview section progress New Files:  - UploadReviewFragment.kt  - upload_default_thumbnail.xml  - fragment_upload_review.xml

---
+++ app/src/main/java/com/pvs/pvsadvisor/upload/UploadReviewFragment.kt
@@ -0,0 +1,18 @@
+package com.pvs.pvsadvisor.upload
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pvs.pvsadvisor.R
+
+/**
+ * Review fragment used to view and edit entered project details
+ */
+class UploadReviewFragment : Fragment(R.layout.fragment_upload_review) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
