33e45e5a146a2c49ed373cfe2ff0627259ccee5f
Steve <sshort95@gmailt.com>
2022-04-21 09:55:59 -0400

    Database:  - User Class

--- app/src/main/java/com/pvs/pvsadvisor/database/User.kt
+++ app/src/main/java/com/pvs/pvsadvisor/database/User.kt
@@ -5,5 +5,8 @@ import io.realm.annotations.PrimaryKey
 
 class User: RealmObject {
     @PrimaryKey var userID: Int = 1
-    var username:String = ""
+    var email:String = ""
+    var password:String = ""
+    var firstName:String = ""
+    var lastName:String = ""
 }
