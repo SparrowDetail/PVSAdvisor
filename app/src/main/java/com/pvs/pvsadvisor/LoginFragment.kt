6300447cb8933b698927b089c2bd72d6c268a6a8
Amar <trek0022@gmail.com>
2022-05-05 05:17:34 -0400

    - Login Fragment successfully implemented after the users' onboarding process. This allows for both 'Sign In' and 'Sign Up' capabilities to be utilized. - Password and email requirements for login procedures.

--- app/src/main/java/com/pvs/pvsadvisor/LoginFragment.kt
+++ app/src/main/java/com/pvs/pvsadvisor/LoginFragment.kt
@@ -13,13 +13,11 @@ import androidx.fragment.app.Fragment
  */
 class LoginFragment : Fragment(R.layout.activity_login)
 {
+    private var isAllFieldsChecked: Boolean = false
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
-
-        // References a email and password edit text
-        val email = view.findViewById<EditText>(R.id.editTextTextEmailAddress)
-        val password = view.findViewById<EditText>(R.id.editTextTextPassword)
 
         // References a login and signup buttons
         val btn_login = view.findViewById<Button>(R.id.b_create)
@@ -27,16 +25,44 @@ class LoginFragment : Fragment(R.layout.activity_login)
 
         // On-click listener to login and take to user main home
         btn_login.setOnClickListener{
-            Toast.makeText(view.context,"Login successful",Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(view.context, MainActivity::class.java)
-            startActivity(intent)
+            isAllFieldsChecked = CheckAllFields();
+            if (isAllFieldsChecked) {
+                Toast.makeText(view.context,"Login successful!",Toast.LENGTH_SHORT).show()
+                val intent = Intent(view.context, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         // On-click listener to sign up page
         btn_signup.setOnClickListener{
+            Toast.makeText(view.context,"Welcome to PVS, start your journey!",Toast.LENGTH_SHORT).show()
             val intent = Intent(view.context, SignupFragment::class.java)
             startActivity(intent)
         }
+    }
+
+    // Checks for credential requirements during the login procedure. Via nullability ammong the
+    // the given strings of information. Still needs a cryptography standard implementation for
+    // internet based access for its database.
+    private fun CheckAllFields(): Boolean {
+
+        val emailInfo = view?.findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val passwordInfo = view?.findViewById<EditText>(R.id.editTextTextPassword)
+
+        if (emailInfo != null) {
+            if (emailInfo.length() === 0) {
+                emailInfo.error = "Email is required"
+                return false
+            }
+        }
+        if (passwordInfo != null) {
+            if (passwordInfo.length() === 0) {
+                passwordInfo.error = "Password is required"
+                return false
+            }
+        }
+        // after all validation return true.
+        return true
     }
 }
--- app/src/main/java/com/pvs/pvsadvisor/OnboardingActivity.kt
+++ app/src/main/java/com/pvs/pvsadvisor/OnboardingActivity.kt
@@ -1,15 +1,22 @@
 package com.pvs.pvsadvisor
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.pvs.pvsadvisor.R
+import com.pvs.pvsadvisor.upload.UploadMain
 import com.ramotion.paperonboarding.PaperOnboardingFragment
 import com.ramotion.paperonboarding.PaperOnboardingPage
 import java.util.ArrayList
 
 class OnboardActivity : AppCompatActivity() {
+    lateinit var getStartedBTN : Button
     private var fragmentManager: FragmentManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +37,19 @@ class OnboardActivity : AppCompatActivity() {
 
         // all the changes are committed
         fragmentTransaction.commit()
+
+        // If user is ready, then he/she can go on from on-boarding to login process.
+        // Clears button sight and transitions overlay of fragment container.
+        val buttonGetStarted : Button = findViewById(R.id.btnGetStarted)
+        buttonGetStarted.setOnClickListener {
+            val loginFragment = LoginFragment()
+            val fragment : Fragment? = supportFragmentManager.findFragmentByTag(loginFragment::class.java.simpleName)
+            if(fragment !is LoginFragment){
+                buttonGetStarted.visibility = View.GONE;
+                supportFragmentManager.beginTransaction().add(R.id.LinearFragment_Container, loginFragment, LoginFragment::class.java.simpleName).commit()
+            }
+        }
+
     }
 
     // the first string is to show the main title ,
@@ -48,28 +68,27 @@ class OnboardActivity : AppCompatActivity() {
             val source = PaperOnboardingPage(
                 "Share Your Vision",
                 "All examples rely on the best enlgish programmers.",
-                Color.parseColor("#EBEBEB"),
+                Color.parseColor("#E0AEFF"),
                 R.drawable.ic_dreameronboard,
                 R.drawable.ic_baseline_search_24
             )
             val source1 = PaperOnboardingPage(
                 "Advice from Professionals",
                 "All examples rely on the best enlgish programmers.",
-                Color.parseColor("#221F20"),
+                Color.parseColor("#D3D3D3"),
                 R.drawable.ic_workingremotelyonboard,
                 R.drawable.ic_baseline_cell_tower_24
             )
             val source2 = PaperOnboardingPage(
                 "The Dream is Now Reality",
                 "All examples rely on the best enlgish programmers.",
-                Color.parseColor("#270B36"),
+                Color.parseColor("#221F20"),
                 R.drawable.ic_teamcollabonboard,
                 R.drawable.ic_baseline_checklist_24
             )
             // array list is used to store
             // data of onbaording screen
             val elements = ArrayList<PaperOnboardingPage>()
-
             // all the sources(data to show on screens)
             // are added to array list
             elements.add(source)
