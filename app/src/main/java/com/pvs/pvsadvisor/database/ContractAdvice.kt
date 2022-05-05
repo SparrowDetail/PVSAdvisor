04eab741f88eed43b971750cfe272d2b973a9915
Steve <sshort95@gmailt.com>
2022-05-04 20:29:10 -0400

    Basic implementation of SQLite Database
    Database:
     - DBHelper.kt
       >Manages database changes and implementation
       >Currently only has user table implementation
     - ContractProject.kt
       >Defines Project Table's key elements
     - ContractUser.kt
       >Defines User Table's key elements
     - ContractProjectImage
     - ContractAdvice.kt
     - ContractProfile.kt
     - ModelUser.kt
       >User data wrapper

--- app/src/main/java/com/pvs/pvsadvisor/database/ContractAdvice.kt
+++ app/src/main/java/com/pvs/pvsadvisor/database/ContractAdvice.kt
@@ -1,4 +1,10 @@
 package com.pvs.pvsadvisor.database
 
 object ContractAdvice {
+    const val TABLE_NAME = "PROJECT_ADVICE"
+    const val PK_ADVICE_ID = "adviceID"
+    const val FK_ADVISOR_ID = "advisorID"
+    const val FK_PROJECT_ID = "projectID"
+    const val KEY_FEEDBACK = "feedback"
+    const val KEy_DOCUMENT_ADDRESS = "documentAddress"
 }
--- app/src/main/java/com/pvs/pvsadvisor/database/ContractProfile.kt
+++ app/src/main/java/com/pvs/pvsadvisor/database/ContractProfile.kt
@@ -1,4 +1,9 @@
 package com.pvs.pvsadvisor.database
 
 object ContractProfile {
+    const val TABLE_NAME = "PROFILE"
+    const val FK_USER_ID = "userID"
+    const val KEY_INTEREST = "interest"
+    const val KEY_GOAL = "goal"
+    const val KEY_WORKS = "works"
 }
